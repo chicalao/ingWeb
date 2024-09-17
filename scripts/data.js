11aa8e9ad1954e5feadc72233639fb3e1aba5674
Laura <laurachica.m@gmail.com>
2024-09-16 22:59:30 -0500

    Primer commit

--- scripts/data.js
+++
@@ -1,5 +0,0 @@
-const productosData = [
-    { id: 1, nombre: 'Pelota de Juguete', tipo: 'Juguete', precio: 10, stock: 5, imagen: 'pelota.jpg', __v: 1 },
-    { id: 2, nombre: 'Vitamina Canina', tipo: 'Medicamento', precio: 20, stock: 3, imagen: 'vitamina.jpg', __v: 1 },
-    // Agrega más productos según necesites
-];
